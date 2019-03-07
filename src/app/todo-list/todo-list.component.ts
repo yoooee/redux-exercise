78a02aaf13cbd9c7c725814a271d2e0d47bfc61c
Josef Staroba <jstaroba@centene.com>
2019-03-07 14:46:51 -0600

    Get Add_Todo to appear on screen

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,24 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TodoService } from "../todo.service";
 import { NgRedux } from "@angular-redux/store";
 import { AppState } from "../store/todo.reducers";
 import { ADD_TODO } from "../store/todo.actions";
+import { Observable } from "rxjs";
+import { Todo } from "../todo.model";
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
+  todoListState: Observable<{todoList: Array<Todo>}>;
   // Read the comment in TodoService
   constructor(private service: TodoService, private ngRedux: NgRedux<AppState>) {
+  }
+
+  ngOnInit() {
+    this.todoListState = this.ngRedux.select('todoList');
   }
 
   addTodo(input) {
     if (!input.value) return;
 
-this.ngRedux.dispatch({ type: ADD_TODO, title: input.value });
-    //this.service.addTodo(input.value);
+    this.ngRedux.dispatch({ type: ADD_TODO, title: input.value });
 
     input.value = '';
   }
